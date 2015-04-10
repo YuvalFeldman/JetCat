82ed315435d017e3ea31c45e3605c2bc41e7241b
David Badalov <caged908716@gmail.com>
2015-04-10 20:27:21 +0300

    Camera Zoom Animation

---
+++ Assets/Scripts/UI/CameraZoom.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraZoom : MonoBehaviour {
+	public Animator animator;
+	private float timer;
+	public bool firstRun;
+
+	// Use this for initialization
+	void Start () {
+		timer = 3;
+		firstRun = true;
+		animator.enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(firstRun){
+			timer -= Time.deltaTime;
+			if ( timer< 0 )
+			{
+				animator.SetBool("Zoom", true);
+				firstRun = false;
+				this.enabled = false;
+			}
+		} else {
+			this.enabled = false;
+		}
+	}
+}
