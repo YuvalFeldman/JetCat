279467ae9dd053f986f8f65c5e6b47a407a4dd73
David Badalov <caged908716@gmail.com>
2015-04-10 21:44:54 +0300

    Menu and Cat Zooms fixed

--- Assets/Scripts/UI/CameraZoom.cs
+++ Assets/Scripts/UI/CameraZoom.cs
@@ -3,16 +3,26 @@ using System.Collections;
 
 public class CameraZoom : MonoBehaviour {
 	public Animator animator;
-	//public bool firstRun;
-	public Canvas menu;
+	public bool firstRun;
+	public Canvas menuCanvas, scoreCanvas;
 	
 	void Start () {
 		animator.enabled = true;
-		menu.enabled = true;
+		scoreCanvas.enabled = false;
+		menuCanvas.enabled = false;
+		firstRun = true;
+	}
+
+	void Update(){
+		if(animator.GetCurrentAnimatorStateInfo(0).IsName("Keep State") && firstRun){
+			menuCanvas.enabled = true;
+		}
 	}
 
 	public void Play () {
 		animator.SetBool("Zoom", true);
-		menu.enabled = false;
+		firstRun = false;
+		scoreCanvas.enabled = true;
+		menuCanvas.enabled = false;
 	}
 }
