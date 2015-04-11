d1fb43f9c999a2bb2f0a65f7ba1f58191164ec5b
David Badalov <caged908716@gmail.com>
2015-04-11 15:48:23 +0300

    Android button fixed

--- Assets/Scripts/LoseScreen.cs
+++ Assets/Scripts/LoseScreen.cs
@@ -13,22 +13,24 @@ public class LoseScreen : MonoBehaviour {
 	}
 	// Use this for initialization
 	public void ShareImage () {
-		string destination = Application.persistentDataPath + "/Screenshot.png";
-		// block to open the file and share it ------------START
-		AndroidJavaClass intentClass = new AndroidJavaClass("android.content.Intent");
-		AndroidJavaObject intentObject = new AndroidJavaObject("android.content.Intent");
-		intentObject.Call<AndroidJavaObject>("setAction", intentClass.GetStatic<string>("ACTION_SEND"));
-		AndroidJavaClass uriClass = new AndroidJavaClass("android.net.Uri");
-		AndroidJavaObject uriObject = uriClass.CallStatic<AndroidJavaObject>("parse","file://" + destination);
-		intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_STREAM"), uriObject);
-		//intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_TEXT"), "testo");
-		//intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_SUBJECT"), "SUBJECT");
-		intentObject.Call<AndroidJavaObject>("setType", "image/jpeg");
-		AndroidJavaClass unity = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
-		AndroidJavaObject currentActivity = unity.GetStatic<AndroidJavaObject>("currentActivity");
-		
-		// option one:
-		currentActivity.Call("startActivity", intentObject);
+		#if UNITY_ANDROID
+			string destination = Application.persistentDataPath + "/Screenshot.png";
+			// block to open the file and share it ------------START
+			AndroidJavaClass intentClass = new AndroidJavaClass("android.content.Intent");
+			AndroidJavaObject intentObject = new AndroidJavaObject("android.content.Intent");
+			intentObject.Call<AndroidJavaObject>("setAction", intentClass.GetStatic<string>("ACTION_SEND"));
+			AndroidJavaClass uriClass = new AndroidJavaClass("android.net.Uri");
+			AndroidJavaObject uriObject = uriClass.CallStatic<AndroidJavaObject>("parse","file://" + destination);
+			intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_STREAM"), uriObject);
+			//intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_TEXT"), "testo");
+			//intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_SUBJECT"), "SUBJECT");
+			intentObject.Call<AndroidJavaObject>("setType", "image/jpeg");
+			AndroidJavaClass unity = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+			AndroidJavaObject currentActivity = unity.GetStatic<AndroidJavaObject>("currentActivity");
+			
+			// option one:
+			currentActivity.Call("startActivity", intentObject);
+		#endif
 	}
 	
 	// Update is called once per frame
