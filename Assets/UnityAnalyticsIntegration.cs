a6fbfaa1cff818ff03ee545e90172535397526ae
FalconPie <blue5blades@gmail.com>
2015-04-12 00:07:59 +0300

    Final commit for the first version, this goes up to the playstore

---
+++ Assets/UnityAnalyticsIntegration.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Cloud.Analytics;
+
+public class UnityAnalyticsIntegration : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		
+		const string projectId = "e1e3a682-0d21-41f3-8368-163da3837405";
+		UnityAnalytics.StartSDK (projectId);
+		
+	}
+	
+}
